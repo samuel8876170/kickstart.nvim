8a25a940413c4fef0955c4cff6ee1918dc64ec49
samuel8876170 <samuel8876170@gmail.com>
2025-05-05 01:16:27 +0800

    add k and q syntax from github katusk/vim-qkdb-syntax

--- init.lua
+++ init.lua
@@ -214,6 +214,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Override file type
+vim.filetype.add {
+  extension = {
+    k = 'k',
+  },
+}
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
