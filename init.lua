8c0073c33e05e47198c201d4b95c46a62b9c794f
samuel8876170 <samuel8876170@gmail.com>
2026-09-12 11:39:53 +0800

    add cpp/h/md 4 space tab

--- init.lua
+++ init.lua
@@ -240,7 +240,7 @@ vim.api.nvim_create_autocmd('FileType', {
 })
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'k', 'q', 's' },
+  pattern = { 'k', 'q', 's', 'cpp', 'h', 'md' },
   callback = function()
     vim.opt_local.tabstop = 4
     vim.opt_local.shiftwidth = 4
